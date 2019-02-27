25154e966886d0fb0595dfcf73cee54c9e6f9ab6
Lanfeng Sun <lanfeng.sun@yahoo.ca>
2019-02-27 11:59:20 -0500

    change address to env variable

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,10 +1,9 @@
 import web3 from "./web3";
-import address from "./address";
 import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  address
+  process.env.ADDRESS
 );
 
 export default instance;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const next = require("next");
 const routes = require("./routes");
 const { createServer } = require("http");
-
+require("dotenv").config();
 const app = next({ dev: process.env.NODE_ENV !== "production", quiet: false });
 const handler = routes.getRequestHandler(app);
 
